78580a526e179c61f5c42973a8de84c3a822e40b
Rob0tWizard <ilia.dementev65@gmail.com>
2023-11-25 00:59:45 +0700

    Part 2: api and endpoints done

--- service/libra/urls.py
+++ service/libra/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import book_list, book_detail
+from .views import book_list, book_detail, BookListCreateView, BookDetailView
 
 urlpatterns = [
     path('books/', book_list, name='book_list'),
     path('book_detail/', book_detail, name='book_detail'),
     path('book_detail/<int:book_id>/', book_detail, name='book_detail'),
+    path('api/books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('api/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 
 ]
--- service/libra/views.py
+++ service/libra/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
 
 
 def book_list(request):
@@ -10,3 +12,12 @@ def book_list(request):
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'libra/book_detail.html', {'book': book})
+
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
