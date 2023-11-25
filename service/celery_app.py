e4bb2a63e535511b5483a9a7cfc720e48de23bf4
Rob0tWizard <ilia.dementev65@gmail.com>
2023-11-25 10:05:06 +0700

    problem solve

--- service/celery_app.py
+++ service/celery_app.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.library.settings')
 
-app = Celery('service')
+app = Celery('service.library')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.broker_url=settings.CELERY_BROKER_URL
+app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
--- service/tasks.py
+++ service/tasks.py
@@ -1,6 +1,6 @@
-from celery_app import shared_task
+from celery import shared_task
 from django.core.mail import send_mail
-from django.contrib.auth import get_user_model
+
 
 from service.clients.models import CustomUser
 
@@ -9,7 +9,7 @@ from service.clients.models import CustomUser
 def welcome_email(user_id):
     user = CustomUser().objects.get(pk=user_id)
     subject = 'Hello!'
-    message = f'Thank you {user.username}, we will have great journey together!'
+    message = f'Thank you {user.username}, we will have a great journey together!'
     from_email = 'email-example@celery.com'
     to_email = [user.email]
 
